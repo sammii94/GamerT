6c8a0010cd2231661f8829e3d42537b4a4435aa3
sammiidelaney <boland.samantha@hotmail.com>
2018-10-03 13:01:32 -0700

    pages made

---
+++ app/src/main/java/com/example/sammiidelaney/gamert/forum.java
@@ -0,0 +1,13 @@
+package com.example.sammiidelaney.gamert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class forum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum);
+    }
+}
